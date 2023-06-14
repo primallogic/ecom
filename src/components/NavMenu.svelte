f732a4028423329f535ccb4ac3738dba98230b82
primallogic <primallogicllc@gmail.com>
2023-06-13 22:12:21 -0400

    updates

---
+++ src/components/NavMenu.svelte
@@ -0,0 +1,17 @@
+<nav id="primaryMenu">
+  <ul>
+    <li>Home</li>
+    <li>Catalog</li>
+    <li>Contact</li>
+  </ul>
+</nav>
+
+<style>
+  #primaryMenu ul {
+    display: inline-flex;
+  }
+
+  #primaryMenu li {
+    padding: 0 10px;
+  }
+</style>
