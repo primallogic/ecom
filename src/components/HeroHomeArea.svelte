751f57e9ac3941cbc44b99c20c7b613d356d0c43
primallogic <primallogicllc@gmail.com>
2023-06-13 23:26:41 -0400

    updates

---
+++ src/components/HeroHomeArea.svelte
@@ -0,0 +1,15 @@
+<script>
+  import heroImage from "../images/heroImage.png";
+</script>
+
+<div id="heroAreaContainer">YOLO</div>
+
+<style>
+  #heroAreaContainer {
+    background-color: #cccccc;
+    height: 400px;
+    width: 100%;
+    background-image: url("../images/heroImage.png");
+    object-fit: cover;
+  }
+</style>
--- src/components/NavMenu.svelte
+++ src/components/NavMenu.svelte
@@ -1,12 +1,23 @@
-<nav id="primaryMenu">
-  <ul>
-    <li>Home</li>
-    <li>Catalog</li>
-    <li>Contact</li>
-  </ul>
-</nav>
+<div id="primaryMenuContainer">
+  <nav id="primaryMenu">
+    <ul>
+      <li>Home</li>
+      <li>About</li>
+      <li>Services</li>
+      <li>Blog</li>
+      <li>Contact</li>
+    </ul>
+  </nav>
+</div>
 
 <style>
+  #primaryMenuContainer {
+    background-color: #000000;
+    color: #ffffff;
+    padding: 5px 0;
+    text-align:center;
+  }
+
   #primaryMenu ul {
     display: inline-flex;
   }
